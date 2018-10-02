5cbfbc687a75690ce2eaeb365438a44e12ed2cf7
DBMG42 <daan-bardoel@outlook.com>
2018-10-02 17:03:15 +0200

    login system with token created

---
+++ src/main/java/nl/han/oose/loginController.java
@@ -0,0 +1,25 @@
+package nl.han.oose;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/")
+public class loginController {
+
+    @POST
+    @Path("/login")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response login(loginCredentials credentials) {
+        if (credentials.getUsername().equals("Daan") && credentials.getPassword().equals("ditiseenheelgeheimpassword1234")) {
+            Token token = new Token(credentials.getUsername(), "1234-1234-1234");
+            return Response.status(Response.Status.OK).entity(token).build();
+        } else {
+            return Response.status(Response.Status.UNAUTHORIZED).build();
+        }
+    }
+}
