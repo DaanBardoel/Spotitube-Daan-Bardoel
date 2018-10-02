8fbbc5dc6b8719b83406610b6cd4e0a4f0b1f01e
DBMG42 <daan-bardoel@outlook.com>
2018-10-02 17:03:51 +0200

    add missing files

---
+++ src/main/java/nl/han/oose/Token.java
@@ -0,0 +1,28 @@
+package nl.han.oose;
+
+public class Token {
+
+    private String token;
+    private String username;
+
+    public Token(String username, String token) {
+        this.username = username;
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
