32e7408b0722bbb205f006d399dfea5e53694ef0
DBMG42 <daan-bardoel@outlook.com>
2018-10-05 09:17:27 +0200

    return value sort of fixed

--- src/main/java/nl/han/oose/loginHandler.java
+++ src/main/java/nl/han/oose/loginHandler.java
@@ -5,12 +5,10 @@ public class loginHandler {
 
     public Token login(LoginCredentials credentials) throws LoginException {
         //create exception with if-else statement
-        Token token;
         if (credentials.getUser().equals("daan") && credentials.getPassword().equals("password")) {
-            token = new Token("1234-1234-1234", credentials.getUser());
+            return new Token("1234-1234-1234", credentials.getUser());
         } else {
             throw new LoginException("credentials not correct!");
         }
-        return token;
     }
 }
